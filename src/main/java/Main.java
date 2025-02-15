a981bbf61406d587605d13af2e2dd2675cd54659
smsm27 <sa27gra10@gmail.com>
2025-02-15 21:25:55 +0100

    Main test creazione

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import view.interfacce.schermata.SchermataCreazione;
+import view.swing.SchermataCreazioneSwing;
+
+public class Main {
+    public static void main(String[] args) {
+        SchermataCreazione view = new SchermataCreazioneSwing();
+        view.inizializza();
+    }
+}
