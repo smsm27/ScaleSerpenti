6e5f9e420bf6cd5c6cdbd02e91752780aef0f4ca
smsm27 <sa27gra10@gmail.com>
2025-02-19 15:05:15 +0100

    test logica

---
+++ src/main/java/MainGioca.java
@@ -0,0 +1,10 @@
+import tools.Colori;
+import view.interfacce.schermata.SchermataGioco;
+import view.swing.SchermataGiocaSwing;
+
+public class MainGioca {
+    public static void main(String[] args) {
+        SchermataGioco schermataGioco =new SchermataGiocaSwing(new Colori());
+        schermataGioco.inizializza();
+    }
+}
